1be627a73b519296dc6c06677f21b8366c419883
dangnguyenn <nguyenminhdang1995@gmail.com>
2020-11-07 14:03:42 -0600

    applied MVVM design pattern

--- BlackKnight/src/main/view_model/MapDriver.java
+++ BlackKnight/src/main/view_model/MapDriver.java
@@ -3,15 +3,15 @@ package main.view_model;
 import main.models.Map;
 
 public class MapDriver {
-    Map map = new Map();
-    public String[][] createPath(int width, int height) {
-        map.setPath(width, height);
-        String[][] path = map.getPath();
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                path[j][i] = ". ";
-            }
-        }
-        return path;
+    Map map;
+
+    public MapDriver(Map map) {
+        this.map = map;
     }
+
+    public String[][] getMapPath(int width, int height) {
+        map.setPath(width, height);
+        return map.getPath();
+    }
+
 }
